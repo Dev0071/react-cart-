2ccbc947945235b99626d3523b3a7aa30d8debbb
Dev0071 <lecturergacheru71@gmail.com>
2023-03-16 15:49:35 +0300

    implented clear cart, and remove item form cart funntionality

--- src/context.js
+++ src/context.js
@@ -1,27 +1,47 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
-import reducer from './reducer'
+import { type } from '@testing-library/user-event/dist/type';
+import React, { useState, useContext, useReducer, useEffect } from 'react';
+import cartItems from './data';
+import reducer from './reducer';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-useReducer-cart-project'
-const AppContext = React.createContext()
+const url = 'https://course-api.com/react-useReducer-cart-project';
+const AppContext = React.createContext();
+
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
 
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // clear cart function
+  const clearCart = () => {
+    dispatch({ type: 'CLEAR_CART' });
+  };
+
+  // remove item from arry of items
+  const removeItem = (id) => {
+    dispatch({ type: 'REMOVE_ITEM', payload: id });
+  };
 
   return (
     <AppContext.Provider
       value={{
-        cart,
+        ...state,
+        clearCart,
+        removeItem,
       }}
     >
       {children}
     </AppContext.Provider>
-  )
-}
+  );
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export { AppContext, AppProvider };
